2dfbd9ef9c561b82daa5f0c04a505e303cf2da98
doomkirov <doomnewera@gmail.com>
2023-01-14 10:25:33 +0300

    admin added, tests removed

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,46 +1,57 @@
 from django.contrib import admin
-from django.contrib.admin.filters import AllValuesFieldListFilter
 
-from .models import Ingridient, Recipe, Tag
-from .forms import RecipeForm
-
-
-class IngridientAdmin(admin.ModelAdmin):
-    list_display = (
-    'pk',
-    'name',
-    'measurement_unit'
+from .models import (
+    Ingridient, Recipe, Tag,
+    RecipeIngridient, Favorite, ShoppingCart
 )
 
 
-class RecipeAdmin(admin.ModelAdmin):
-    form = RecipeForm
-    list_filter = ('tags',)
-    list_display = (
-        'author',
-        'name',
-        'image',
-        'text',
-        'time',
-    )
-    list_editable = (
-        'name',
-        'time'
-    )
-    search_fields = (
-        'name',
-    )
+class IngridientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
 
 
 class TagAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'name',
-        'hexcolor',
-        'slug'
+    list_display = ('name', 'color', 'slug')
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'pub_date', 'display_tags', 'favorite')
+    list_filter = ('name', 'author', 'tags')
+    search_fields = ('name',)
+    readonly_fields = ('favorite',)
+    fields = (
+        'image',
+        ('name', 'author'),
+        'text',
+        ('tags', 'cooking_time'),
+        'favorite'
     )
+
+    def display_tags(self, obj):
+        return ', '.join([tag.name for tag in obj.tags.all()])
+    display_tags.short_description = 'Теги'
+
+    def favorite(self, obj):
+        return obj.favorite.count()
+    favorite.short_description = 'Тег в избранном'
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount')
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'user')
+
+
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'user')
 
 
 admin.site.register(Ingridient, IngridientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Tag, TagAdmin)
+admin.site.register(RecipeIngridient, RecipeIngredientAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,26 +1,18 @@
 from django.contrib import admin
-from django import forms
-from django.contrib.auth.forms import AdminPasswordChangeForm
+from django.contrib.admin import ModelAdmin
+from django.contrib.auth.admin import UserAdmin
 
-from .models import User
-
-
-class UserChangeForm(forms.ModelForm):
-    password = AdminPasswordChangeForm
-
-    class Meta:
-        model = User
-        fields = ('password',)
+from .models import User, Follow
 
 
-class UserAdmin(admin.ModelAdmin):
-    form = UserChangeForm
-    list_display = (
-        'username',
-        'first_name',
-        'last_name',
-        'email',
-    )
+class CustomUserAmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('username', 'email')
+
+
+class FollowAdmin(ModelAdmin):
+    list_display = ('user', 'author')
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Follow, FollowAdmin)
